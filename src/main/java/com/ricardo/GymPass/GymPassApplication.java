9b86b9b1d4eefcda79f683d903017ce804661cd1
Ricardo Oliveira <ricardoliveira5ro@gmail.com>
2026-03-03 17:39:54 +0000

    first commit

---
+++ src/main/java/com/ricardo/GymPass/GymPassApplication.java
@@ -0,0 +1,13 @@
+package com.ricardo.GymPass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymPassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymPassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ricardo/GymPass/GymPassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricardo.GymPass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymPassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
